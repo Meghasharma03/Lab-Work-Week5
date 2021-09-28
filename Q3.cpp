60b548162427023aa32c125f10866a584d7882ac
Meghasharma03 <88472829+Meghasharma03@users.noreply.github.com>
2021-09-28 17:04:45 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,34 @@
+//Megha Sharma
+//You Have Been Given Two sorted integer arrays of size m and n. Design an algorithm and implement it using a program to find list of element which are common to both .(Time Complexity=0(m+n)
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a1[n];
+	for(int i=0;i<n;i++)
+	cin>>a1[i];
+	int m;
+	cin>>m;
+	int a2[m];
+	for(int i=0;i<m;i++)
+	cin>>a2[i];
+	int i=0,j=0;
+	sort(a1,a1+n);
+	sort(a2,a2+m);
+	while(i!=n&& j!=m)
+	{
+		if(a1[i]==a2[j])
+		{
+			cout<<a1[i]<<" ";
+			i++;
+			j++;
+		}
+		else if(a1[i]<a2[j])
+		i++;
+		else
+		j++;
+	}
+	return 0;
+ } 
